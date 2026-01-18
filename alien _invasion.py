0e1814b35764163968ce77f1fddfba45038fbc0e
Homayoun <homayoundbd@gmail.com>
2026-01-18 17:41:24 +0100

    frame rate, main class, background color

---
+++ alien _invasion.py
@@ -0,0 +1,33 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    # managing game assets and behavior
+    
+    def __init__(self):
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+        
+        self.clock = pygame.time.Clock()
+        
+        # background color
+        self.bg_color = (230, 230, 230)
+        
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    
+            pygame.display.flip()
+            self.clock.tick(60)
+            
+            # background of game
+            self.screen.fill(self.bg_color)
+            
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
